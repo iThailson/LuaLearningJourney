e27410569e8e164db44dabe3e0be0ae30cc2df2d
Nosliaht <thailson-games-2002@hotmail.com>
2025-12-16 02:34:00 -0300

    02 lesson of the moon course

---
+++ Module/01/classroom02.lua
@@ -0,0 +1,30 @@
+local Value01 = "Hello, world!";
+local Value02 = "Other information.";
+
+print(Value01);
+print(Value02);
+
+print("\n");
+
+local n; -- nil
+local n1 =20; -- number
+local n2 = "text"; -- string
+local  n3 = true; -- boolean
+
+print(n);
+print(n1);
+print(n2);
+print(n3);
+
+print("\n");
+
+print(type(n));
+print(type(n1));
+print(type(n2));
+print(type(n3));
+
+print("\n");
+
+local value03 = "20";
+local type = type(value03);
+print(type);
