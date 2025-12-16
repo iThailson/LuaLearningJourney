81cd7017ba5966faf5846c93524e5449f39e5ffc
Nosliaht <thailson-games-2002@hotmail.com>
2025-12-16 05:54:10 -0300

    04 lesson of the moon course

---
+++ Module/01/classroom04.lua
@@ -0,0 +1,31 @@
+print(5 + 5)
+print("\n")
+
+local n1 = 5
+local n2 = 10
+print(n1 + n2)
+print("\n")
+
+local n3 = 10
+local n4 = 20
+local sum = n3 + n4
+print(sum)
+print("\n")
+
+print(10 + 10)
+print(-5)
+print(5 / 2)
+print(6 * 2)
+print(10 - 2)
+print(10 % 2)
+print("\n")
+
+sum = 5+10*5
+print(sum)
+print("\n")
+
+sum = (5+10)*5
+print(sum)
+print("\n")
+
+print(10 ^ 2)
