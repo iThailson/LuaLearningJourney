a7f1d4b3191ffe9b69fa03a160ab33763e5d2522
Nosliaht <thailson-games-2002@hotmail.com>
2025-12-18 07:07:06 -0300

    08 lesson of the moon course

---
+++ Module/01/classroom08.lua
@@ -0,0 +1,12 @@
+local string1 = "Hello,"
+local string2 = "World!"
+local stringJoining1 = string1 .. " " .. string2
+print(stringJoining1, #stringJoining1)
+local string3 = "Hello, "
+local string4 = "World!"
+local stringJoining2 = string3 .. string4
+print(stringJoining2, #stringJoining2)
+print("Hello,\nWorld!")
+print("Hello,\tWorld!")
+print("\"Hello, World!\"")
+print("\\Hello, World!\\")
