e124bdc7c21b4d81aa1a77bad02945478a8b25fa
Nosliaht <thailson-games-2002@hotmail.com>
2025-12-20 14:01:40 -0300

    07 lesson from the moon course - Second Module

---
+++ Module/02/classroom07.lua
@@ -0,0 +1,10 @@
+::start::
+print("Enter a number (0-10): ")
+local number = io.read("*n")
+
+if number > 10 or number < 0 then
+  print(string.format("The number %d is out of the limit.", number))
+  goto start
+elseif number <= 10 or number >= 0 then
+  print(string.format("The number %d is at the limit.", number))
+end
