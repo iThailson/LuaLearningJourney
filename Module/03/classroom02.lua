2621728b6aa4c9e5c3f74df243aa885206009600
Nosliaht <thailson-games-2002@hotmail.com>
2025-12-26 16:56:00 -0300

    02 lesson from the moon course - 03 Module

---
+++ Module/03/classroom02.lua
@@ -0,0 +1,12 @@
+local matrix = { { 10, 20, 30 }, { "Hello", "World" }, { 100, 200, 300 } }
+
+print(matrix[2][1])
+
+local set = {}
+set["apple"] = true
+set["banana"] = true
+set["cherry"] = true
+
+if set["apple"] then
+  print("Exist")
+end
